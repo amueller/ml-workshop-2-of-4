c336ccf0d50bf84fd042e852d67ba3c407f283e5
Andreas Mueller <andreas.mueller@columbia.edu>
2019-11-04 15:52:11 -0800

    update bike regression to use column transformer and new ohe

--- notebooks/solutions/bike_regression.py
+++ notebooks/solutions/bike_regression.py
@@ -6,16 +6,9 @@ display(data.head())
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
-from sklearn.preprocessing import OneHotEncoder
-ohe = OneHotEncoder(sparse=False)
-
-X_train_ohe = ohe.fit_transform(X_train)
-X_test_ohe = ohe.transform(X_test)
-
 from sklearn.linear_model import LinearRegression
 
-
-# scale here
+# for other models you should scale here
 
 lr = LinearRegression().fit(X_train, y_train)
 
@@ -27,6 +20,21 @@ from sklearn.metrics import mean_squared_error
 y_pred = lr.predict(X_test)
 print(mean_squared_error(y_test, y_pred))
 
+
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import OneHotEncoder
+ohe = make_column_transformer(
+    (OneHotEncoder(sparse=False), X_train.columns[:6]),
+    remainder='passthrough')
+
+X_train_ohe = ohe.fit_transform(X_train)
+X_test_ohe = ohe.transform(X_test)
+
+X_train.shape
+
+X_train_ohe.shape
+
+
 lr = LinearRegression().fit(X_train_ohe, y_train)
 
 print(lr.score(X_train_ohe, y_train))
@@ -35,4 +43,3 @@ print(lr.score(X_test_ohe, y_test))
 
 from sklearn.metrics import mean_squared_error
 y_pred = lr.predict(X_test_ohe)
-print(mean_squared_error(y_test, y_pred))
